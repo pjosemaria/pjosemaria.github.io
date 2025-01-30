21dcd9458e0cc2b7da546f2b93ac910dd83863d0
pjosemaria <pjosemaria757@gmail.com>
2025-01-30 15:52:43 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('directo-btn').addEventListener('click', function() {
+    alert('Accediendo a la transmisión en vivo...');
+    // Aquí puedes agregar la lógica para mostrar el video en directo
+});
