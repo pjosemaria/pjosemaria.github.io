9f6201e49e96fc68c89d99e7bb4955c7a27b6ce8
pjosemaria <pjosemaria757@gmail.com>
2025-01-30 01:40:32 -0600

    Delete Amate TV directory

--- Amate TV/scripts.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('directo-btn').addEventListener('click', function() {
-    alert('Accediendo a la transmisión en vivo...');
-    // Aquí puedes agregar la lógica para mostrar el video en directo
-});
